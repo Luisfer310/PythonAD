7808a3030f956874ae0c3a4d8f0808c319eba6fd
Luisfer310 <her.osuna3@gmail.com>
2024-02-09 19:31:34 -0700

    Ultimo guardado 09/02/24 19:31 hrs

---
+++ Programas como ejercicios/JuegoAleatorio.py
@@ -0,0 +1,29 @@
+from random import *
+
+# Juego de adivinar el numero
+numero = randint(1, 100)
+vidas = 8
+contador = 0
+while vidas > 0:
+    try:
+        numerousa = int(input('Dame un numero: '))
+        if numerousa == numero:
+            print('¡Ganaste!'.center(50, '-'))
+            print('El numero que ingresaste es igual al numero alateorio')
+            break
+        else:
+            contador += 1
+            vidas -= 1
+            if numerousa > numero:
+                print('El numero que ingresaste es incorrecto, es mayor a el numero aleatorio')
+            elif numerousa < numero:
+                print('El numero que ingresste es incorrecto, es menor a el numero aleatorio')
+
+    except:
+        print('Dato erroneo.')
+        pass
+
+if vidas > 1:
+    print(f'Te quedaron {vidas} vidas y lo lograste en el intento {contador}')
+elif vidas <= 1:
+    print(f'Te quedo {vidas} vidas y lo lograste en el intento {contador} ')
